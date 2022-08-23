61c6206a00bac86967f80d0a56e250daa5de56ee
fotis <f.stamatopoulos@gmail.com>
2022-08-23 16:17:22 +0300

    Video Player

---
+++ script.js
@@ -0,0 +1,111 @@
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const Stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
+
+
+
+
+
+
+
+
+
+
+//Play and pause video
+
+function toggleVideoStatus(){
+
+   if(video.paused){
+    
+    video.play();
+
+   }
+   else{
+
+    video.pause();
+
+   }
+
+}
+
+//Update play/pause icon
+
+function updatePlayIcon(){
+
+    if(video.paused){
+ 
+        play.innerHTML = '<i class = "fa fa-play fa-2x"></i>';
+
+    }
+    else{
+
+        play.innerHTML = ' <i class = " fa fa-stop fa-2x"></i>';                   
+
+    }
+}
+
+//Update progress and timestamp
+
+function updateProgress(){
+
+    progress.value = (video.currentTime/video.duration)*100 ;
+
+    //Get minutes
+    let mins = Math.floor(video.currentTime/60);
+
+    if(mins<10){
+
+        mins = "0" + String(mins);
+
+    }
+
+    //Get seconds
+    let secs = Math.floor(video.currentTime%60);
+
+    if(secs<10){
+
+        secs = "0" + String(secs);
+
+    }
+    timestamp.innerHTML = `${mins}:${secs}`;
+}
+
+//Set video time to progress
+
+function setVideoProgress(){
+
+    video.currentTime = (+progress.value * video.duration)/100;
+
+}
+
+//Stop the video
+
+function stopVideo(){
+
+    video.currentTime = 0;
+    video.pause();
+
+}
+
+
+//Event Listeners
+
+video.addEventListener("click", toggleVideoStatus);   //this function, play the video if its paused or stoped ,and paused the video if its playing
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("timeupdate", updateProgress);  
+
+play.addEventListener("click", toggleVideoStatus);
+
+Stop.addEventListener("click", stopVideo);
+
+progress.addEventListener("change", setVideoProgress);
+
+
+
+
+
+
+
